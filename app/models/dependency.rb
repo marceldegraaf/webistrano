d1c96100b89521a93f1a446b0ceac5ec4f43e683
Marcel de Graaf <marcel@slashdev.nl>
2009-10-31 13:53:51 +0100

    Add dependencies to stages in views

--- app/models/dependency.rb
+++ app/models/dependency.rb
@@ -1,3 +1,9 @@
 class Dependency < ActiveRecord::Base
   belongs_to :stage
+
+  validates_presence_of :name, :current_version, :available_version
+
+  def updateable?
+    available_version > current_version
+  end
 end
--- test/unit/dependency_test.rb
+++ test/unit/dependency_test.rb
@@ -1,8 +1,19 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
 
 class DependencyTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def test_validations
+    assert_equal 0, Stage.count
+
+    d = Dependency.new
+
+    assert !d.valid?
+    assert_not_nil d.errors.on('name')
+  end
+
+  def test_updateable
+    d = Dependency.new(:name => 'haml', :current_version => '1.0.0', :available_version => '1.1.0')
+    d2 = Dependency.new(:name => 'haml', :current_version => '1.0.0', :available_version => '0.9')
+    assert_equal d.updateable?, true
+    assert_equal d2.updateable?, false
   end
 end
